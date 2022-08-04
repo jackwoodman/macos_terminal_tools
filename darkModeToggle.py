4fa1a6888245bf0780f2408701cdcb91d729dc97
jdwoodman <jack.woodman@student.unimelb.edu.au>
2022-08-04 11:25:00 +1000

    updated dark mode description

--- darkModeToggle.py
+++ darkModeToggle.py
@@ -1,3 +1,9 @@
 # dark mode
 import os
-os.system("osascript -e 'tell app \"System Events\" to tell appearance preferences to set dark mode to not dark mode'")
+
+# this is super simple and a little hacky, makes use of the fact that the apple
+# script command to turn *on* dark mode will turn it *off* if already on
+# (and obviously turn *on* if already *off*)
+
+COMMAND_TOGGLE_DRK_MD = "osascript -e 'tell app \"System Events\" to tell appearance preferences to set dark mode to not dark mode'"
+os.system(COMMAND_TOGGLE_DRK_MD)
